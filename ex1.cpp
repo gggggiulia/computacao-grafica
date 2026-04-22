cf97b1a40df31c95da1c4e56eb9fc3c3e406c52c
giulia <169063222+gggggiulia@users.noreply.github.com>
2026-04-22 14:24:45 -0300

    Add files via upload

---
+++ ex1.cpp
@@ -0,0 +1,57 @@
+#include <GLUT/glut.h>
+#include <OpenGL/gl.h>
+
+float x = 0.0f;
+float y = 0.0f;
+float step = 0.1f;
+
+void display(){
+    glClear(GL_COLOR_BUFFER_BIT);
+    glLoadIdentity();
+
+    glTranslatef(x, y, 0.0f);
+    glColor3f(1.0f, 0.2f, 0.5f); //Rosa!
+    glutSolidSphere(0.2, 25, 25);
+
+    glutSwapBuffers();
+}
+
+void keyboard(unsigned char tecla, int xMouse, int yMouse){
+    switch(tecla){
+        case 'w':
+            y += step;
+            break;
+        case 's':
+            y -= step;
+            break;
+        case 'a':
+            x -= step;
+            break;
+        case 'd':
+            x += step;
+            break;
+    }
+    glutPostRedisplay(); //Redesenhar a cada uso das teclas
+}
+
+void init() {
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-1.0, 1.0, -1.0, 1.0);
+}
+
+int main(int argc, char** argv){
+    glutInit(&argc, argv);
+    int mode = GLUT_DOUBLE | GLUT_RGB;
+    glutInitDisplayMode(mode);
+    glutInitWindowSize(500,500);
+    glutCreateWindow("Círculo para o teclado");
+
+    init();
+
+    glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard);
+    glutMainLoop();
+    return 0;
+}
---
+++ ex4.cpp
@@ -0,0 +1,82 @@
+#include <GLUT/glut.h>
+#include <OpenGL/gl.h>
+
+float x = 0.0f;
+float y = 0.0f;
+float step = 0.1f;
+
+float r = 1.0f, g = 0.2f, b = 0.5f;
+
+void display(){
+    glClear(GL_COLOR_BUFFER_BIT);
+    glLoadIdentity();
+
+    glTranslatef(x, y, 0.0f);
+    glColor3f(r, g, b);
+    
+    glBegin(GL_QUADS);
+        glColor3f(r, g, b);
+        glVertex2f(0.1f, 0.1f);
+        glVertex2f(-0.1f, 0.1f);
+        glVertex2f(-0.1f, -0.1f);
+        glVertex2f(0.1f, -0.1f);
+    glEnd();
+
+    glutSwapBuffers();
+}
+
+void specialKeys(int tecla, int xMouse, int yMouse){
+    switch(tecla){
+        case GLUT_KEY_UP:
+            if(y < 0.9) y += step;
+            break;
+        case GLUT_KEY_DOWN:
+            if(y > -0.9) y -= step;
+            break;
+        case GLUT_KEY_LEFT:
+            if(x > -0.9) x -= step;
+            break;
+        case GLUT_KEY_RIGHT:
+            if(x < 0.9) x += step;
+            break;
+
+        case '1':
+            r = 0.1f; 
+            g = 0.5f; 
+            b = 0.9f;
+            break;
+        case '2':
+            r = 0.2f; 
+            g = 0.8f; 
+            b = 0.4f;
+            break;
+        case '3':
+            r = 1.0f; 
+            g = 0.2f; 
+            b = 0.5f;
+            break;
+    }
+    glutPostRedisplay(); //Redesenhar a cada uso das teclas
+}
+
+void init() {
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-1.0, 1.0, -1.0, 1.0);
+}
+
+int main(int argc, char** argv){
+    glutInit(&argc, argv);
+    int mode = GLUT_DOUBLE | GLUT_RGB;
+    glutInitDisplayMode(mode);
+    glutInitWindowSize(500,500);
+    glutCreateWindow("Círculo para o teclado");
+
+    init();
+
+    glutDisplayFunc(display);
+    glutSpecialFunc(specialKeys);
+    glutMainLoop();
+    return 0;
+}
